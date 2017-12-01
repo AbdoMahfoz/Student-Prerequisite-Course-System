f60a66fab6c428533f1641ba311068b5847f3e4c
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2017-12-01 15:35:10 +0200

    Converted CourseTree to Generic Tree

--- Student prerequisite course system/DataStructures/Tree.cs
+++ Student prerequisite course system/DataStructures/Tree.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 
-public class CourseTree
+public class Tree<T> where T : ITreeElement
 {
     //private
-    ArrayList<Course> Courses;
+    ArrayList<T> Elements;
     ArrayList<ArrayList<int>> AdjacencyList;
     int count;
     int capacity;
@@ -13,15 +13,15 @@ public class CourseTree
         {
             if (n < 0 || n >= AdjacencyList.Count)
             {
-                throw new IndexOutOfRangeException("Course is not added to tree");
+                throw new IndexOutOfRangeException();
             }
         }
     }
     //public
-    public CourseTree()
+    public Tree()
     {
         AdjacencyList = new ArrayList<ArrayList<int>>();
-        Courses = new ArrayList<Course>();
+        Elements = new ArrayList<T>();
         count = 0;
         capacity = 0;
     }
@@ -29,34 +29,34 @@ public class CourseTree
     {
         get => count;
     }
-    public void AddCourse(Course c)
+    public void AddCourse(T value)
     {
-        c.TreeIndex = count;
-        Courses.Append(c);
+        value.TreeIndex = count;
+        Elements.Append(value);
         AdjacencyList.Count++;
     }
-    public void Connect(Course dependant, Course dependee)
+    public void Connect(T dependant, T dependee)
     {
         CheckIndex(dependant.TreeIndex, dependee.TreeIndex);
-        if(AdjacencyList[dependant.TreeIndex] == null)
+        if (AdjacencyList[dependant.TreeIndex] == null)
         {
             AdjacencyList[dependant.TreeIndex] = new ArrayList<int>();
         }
         AdjacencyList[dependant.TreeIndex].Append(dependee.TreeIndex);
     }
-    public Course[] GetDependantCourses(Course Target, Course[] AlreadyTaken)
+    public T[] GetDependantCourses(T Target, T[] AlreadyTaken)
     {
-        ArrayList<Course> res = new ArrayList<Course>();
-        ArrayList<Course> queue = new ArrayList<Course>();
+        ArrayList<T> res = new ArrayList<T>();
+        ArrayList<T> queue = new ArrayList<T>();
         queue.Append(Target);
-        while(queue.Count > 0)
+        while (queue.Count > 0)
         {
-            Course tmp = queue[Count - 1];
+            T tmp = queue[Count - 1];
             queue.PopBack();
-            for(int i = 0; AdjacencyList[tmp.TreeIndex] != null && i < AdjacencyList[tmp.TreeIndex].Count; i++)
+            for (int i = 0; AdjacencyList[tmp.TreeIndex] != null && i < AdjacencyList[tmp.TreeIndex].Count; i++)
             {
-                Course tmpTarget = Courses[AdjacencyList[tmp.TreeIndex][i]];
-                if(!Array.Exists(AlreadyTaken, new Predicate<Course>((Course a) => { return a == tmpTarget; })))
+                T tmpTarget = Elements[AdjacencyList[tmp.TreeIndex][i]];
+                if (!Array.Exists(AlreadyTaken, new Predicate<T>((T a) => { return a == tmpTarget; })))
                 {
                     queue.Append(tmpTarget);
                     res.Append(tmpTarget);
@@ -67,37 +67,49 @@ public class CourseTree
     }
     public string[] UnloadToFile()
     {
-        ArrayList<string> res = new ArrayList<string>();
-        System.Text.StringBuilder tmp = new System.Text.StringBuilder();
-        for(int i = 0; i < count; i++)
+        if (typeof(T) == typeof(Course))
         {
-            tmp.Append(Courses[i].Name + " ");
-            if (AdjacencyList[i] == null) continue;
-            for(int j = 0; j < AdjacencyList[i].Count; j++)
+            ArrayList<string> res = new ArrayList<string>();
+            System.Text.StringBuilder tmp = new System.Text.StringBuilder();
+            for (int i = 0; i < count; i++)
             {
-                tmp.Append(AdjacencyList[i][j] + " ");
+                Course c = Elements[i] as Course;
+                tmp.Append(c.Name + " ");
+                if (AdjacencyList[i] == null) continue;
+                for (int j = 0; j < AdjacencyList[i].Count; j++)
+                {
+                    tmp.Append(AdjacencyList[i][j] + " ");
+                }
+                tmp.Remove(tmp.Length - 1, 1);
+                res.Append(tmp.ToString());
+                tmp.Clear();
             }
-            tmp.Remove(tmp.Length - 1, 1);
-            res.Append(tmp.ToString());
-            tmp.Clear();
+            return res.ToArray();
         }
-        return res.ToArray();
+        else
+        {
+            return null;
+        }
     }
     public void LoadFromFile(string[] filedata)
     {
-        AdjacencyList.Count = filedata.Length;
-        for(int i = 0; i < filedata.Length; i++)
+        if (typeof(T) == typeof(Course))
         {
-            AdjacencyList[i] = new ArrayList<int>();
-            string[] fields = filedata[i].Split(' ', '\0');
-            if (fields.Length <= 1) continue;
-            Course c = FileOperations.CoursesFile.GetCourse(fields[0]);
-            c.TreeIndex = i;
-            Courses.Append(c);
-            for(int j = 1; j < fields.Length; j++)
+            AdjacencyList.Count = filedata.Length;
+            for (int i = 0; i < filedata.Length; i++)
             {
-                AdjacencyList[i].Append(int.Parse(fields[j]));
+                AdjacencyList[i] = new ArrayList<int>();
+                string[] fields = filedata[i].Split(' ', '\0');
+                if (fields.Length <= 1) continue;
+                Course c = FileOperations.CoursesFile.GetCourse(fields[0]);
+                c.TreeIndex = i;
+                Elements.Append(c as T);
+                for (int j = 1; j < fields.Length; j++)
+                {
+                    AdjacencyList[i].Append(int.Parse(fields[j]));
+                }
             }
         }
     }
 }
+
