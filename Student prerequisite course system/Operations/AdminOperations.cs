c4373d2a9c4ca7dfcbf008b9d0f45b94f71b6781
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2017-12-10 17:01:40 +0200

    Folder-Structure Changes

--- Student prerequisite course system/Operations/AdminOperations.cs
+++ Student prerequisite course system/Operations/AdminOperations.cs
@@ -1,14 +1,6 @@
-﻿
+﻿using System;
+
 static public class AdminOperations
 {
-    static public Student GetStudent(int StudentID)
-    {
-        Student s = FileOperations.UsersFile.GetUser(StudentID);
-        s.RegisteredCourses = FileOperations.Users_SubjectsFile.GetSubjects(s);
-        return s;
-    }
-    static public ArrayList<Student> GetAllStudentsInCourse(Course c)
-    {
-        return FileOperations.Subjects_UsersFile.GetStudents(c);
-    }
+
 }
