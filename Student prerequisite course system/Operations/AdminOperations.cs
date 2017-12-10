f56876f2161dd644812759899d057ff53a0ad6e1
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2017-12-10 15:44:11 +0200

    First steps in AdminOperations

---
+++ Student prerequisite course system/Operations/AdminOperations.cs
@@ -0,0 +1,14 @@
+﻿
+static public class AdminOperations
+{
+    static public Student GetStudent(int StudentID)
+    {
+        Student s = FileOperations.UsersFile.GetUser(StudentID);
+        s.RegisteredCourses = FileOperations.Users_SubjectsFile.GetSubjects(s);
+        return s;
+    }
+    static public ArrayList<Student> GetAllStudentsInCourse(Course c)
+    {
+        return FileOperations.Subjects_UsersFile.GetStudents(c);
+    }
+}
