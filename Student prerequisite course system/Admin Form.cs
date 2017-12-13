6f7909b131f50e00666c9564c38f466c8dee54c6
Ranakhamis <ranakhms@gmail.com>
2017-12-13 23:55:37 +0200

    admin basic design

---
+++ Student prerequisite course system/Admin Form.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace Student_prerequisite_course_system
+{
+    public partial class MainForm : Form
+    {
+        Tree<Course> Courses;
+        Student ActiveStudent;
+        public MainForm()
+        {
+            Courses = new Tree<Course>();
+            Courses.LoadFromFile(FileOperations.TreeFile.Read());
+            ActiveStudent = null;
+            InitializeComponent();
+        }
+    }
+}
